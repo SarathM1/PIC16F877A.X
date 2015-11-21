b3e9e6d26a3178cc433d28c65dabe61cb8b81a04
Wirewords-PC <sarathm00@gmail.com>
2015-11-21 16:39:40 +0530

    Testing Led

--- pgm1.c
+++ pgm1.c
@@ -1,9 +1,32 @@
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
 #include<pic16f877a.h>
 int main()
 {
-    unsigned char i = 0;
+    int i = 0;
     TRISB = 0;
-    for(i=0;i<0xff;i++)
-        PORTB = i;
-    while(1);
+    while(1)
+    {
+        for(i=0;i<=255;i++)
+        {
+            PORTB = i;
+            __delay_ms(50);
+        }
+
+        for(;i>=0;i--)
+        {
+            PORTB = i;
+            __delay_ms(50);
+        }
+    }
 }
